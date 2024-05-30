411b9473a0c5d6a82c61fce0905574e8bcd4a3c4
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2024-05-30 09:17:31 +0200

    Update Main.js

--- Web/JS/Main.js
+++ Web/JS/Main.js
@@ -1,7 +1,13 @@
 function showSideBar() {
-  document.querySelector('.sidebar').classList.add('open');
-}
+            document.querySelector('.sidebar').classList.add('open');
+            document.getElementById('overlay').classList.add('show');
+            document.getElementById('content').classList.add('shift');
+        }
 
-function hideSideBar() {
-  document.querySelector('.sidebar').classList.remove('open');
-}
+        function hideSideBar() {
+            document.querySelector('.sidebar').classList.remove('open');
+            document.getElementById('overlay').classList.remove('show');
+            document.getElementById('content').classList.remove('shift');
+        }
+
+        document.getElementById('overlay').addEventListener('click', hideSideBar);
