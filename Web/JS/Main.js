559bbd646e33402ca01683b8b08f38b7d57a60f3
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2024-07-25 07:17:40 +0000

    hello

--- Web/JS/Main.js
+++
@@ -1,7 +0,0 @@
-function showSideBar() {
-  document.querySelector('.sidebar').classList.add('open');
-}
-
-function hideSideBar() {
-  document.querySelector('.sidebar').classList.remove('open');
-}
